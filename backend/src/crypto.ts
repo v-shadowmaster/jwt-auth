7111d635084c83aac3469a9c3379c87e007622da
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-11-09 00:10:20 +0530

    implemented the crypto funntions for hashing

--- backend/src/crypto.ts
+++ backend/src/crypto.ts
@@ -1,5 +1,19 @@
 import bcrypt from "bcrypt";
 
-export async function hashPassword(password: string): string {
-    return "asdf"
+export async function hashPassword(password: string) {
+    return bcrypt.hash(password, 12);
+}
+
+
+export async function verifyPassword(password: string, hash: string) {
+    return bcrypt.compare(password, hash);
+}
+
+export async function hashOpaqueToken(token: string) {
+    return bcrypt.hash(token, 12);
+}
+
+
+export async function compareHashOpaqueToken(token: string, hash: string) {
+    return bcrypt.compare(token, hash);
 }
